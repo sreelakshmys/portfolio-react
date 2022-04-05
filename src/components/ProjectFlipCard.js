4ffe518f588f8d2981cb174495dbfa3970a56037
Sreelakshmy <sreelakshmys.mec@gmail.com>
2022-04-05 15:24:32 +0200

    Adding links to projects

--- src/components/ProjectFlipCard.js
+++ src/components/ProjectFlipCard.js
@@ -3,20 +3,31 @@ import "../scss/App.scss";
 
 export default function ProjectFlipCard({ projectData, t }) {
   return (
-    <div className="flip-outer-div">
-      <div className="flip-card">
-        <div className="flip-card-inner">
-          <div className="flip-card-front">
-            <img className="project-image" src={projectData.img} alt="Avatar" />
-          </div>
-          <div className="flip-card-back">
-            <div>
-              <h1 className="project-title">{t(projectData.title)}</h1>
-              <p>{t(projectData.about)}</p>
+    <a
+      href={projectData.url}
+      alt="project"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <div className="flip-outer-div">
+        <div className="flip-card">
+          <div className="flip-card-inner">
+            <div className="flip-card-front">
+              <img
+                className="project-image"
+                src={projectData.img}
+                alt="Avatar"
+              />
+            </div>
+            <div className="flip-card-back">
+              <div>
+                <h1 className="project-title">{t(projectData.title)}</h1>
+                <p>{t(projectData.about)}</p>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </a>
   );
 }
